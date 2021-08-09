422931fe4c9a403235391185d00668ec252b3ebf
Mariia Kornieva <mariiakornieva@gmail.com>
2021-08-09 10:44:55 +0300

    Add onStartStop event handler

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,66 @@
 import * as _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const SEC_IN_MIN = 60;
+const FOCUS_TIMEOUT = 50 * SEC_IN_MIN;
+const REST_TIMEOUT = 10 * SEC_IN_MIN;
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('text-red-700');
+const timer = document.getElementById('focus-timer');
+const startStopButton = document.getElementById('start-stop-btn');
+// const focusRestButton = document.getElementById();
 
-  return element;
-}
+const isInProgress = false;
+let timerId: NodeJS.Timer;
 
-document.body.appendChild(component());
+type Time = {
+  min: number;
+  sec: number;
+};
+
+const decrementTimeout = (time: Time): Time => {
+  if (time.sec === 0) {
+    return {
+      min: time.min - 1,
+      sec: 99,
+    };
+  }
+
+  return {
+    min: time.min,
+    sec: time.sec - 1,
+  };
+};
+
+const onStartStop = (event: Event) => {
+  if (isInProgress) {
+    clearInterval(timerId);
+
+    return;
+  }
+
+  timerId = setInterval(() => {
+    if (!timer) {
+      return;
+    }
+
+    const currentTime = timer.textContent;
+
+    if (!currentTime) {
+      clearInterval(timerId);
+      return;
+    }
+
+    const min = parseInt(currentTime);
+    const sec = Number(currentTime.slice(currentTime.indexOf(':') + 1));
+
+    if (min + sec <= 1) {
+      clearInterval(timerId);
+      return;
+    }
+
+    const newTime = decrementTimeout({ min, sec });
+    timer.textContent = `${newTime.min}:${newTime.sec}`;
+  }, 1000);
+};
+
+startStopButton?.addEventListener('click', onStartStop);
