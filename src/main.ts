783b0bb164b9edbfbb7fbce46accb6dff7b4644f
Mariia Kornieva <mariiakornieva@gmail.com>
2021-08-09 09:18:06 +0300

    Remote unused class

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,12 @@ import * as _ from 'lodash';
 import './style.css';
 
 function component() {
-    const element = document.createElement('div');
+  const element = document.createElement('div');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('text-red-700');
-    element.classList.add('bg-red-100');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('text-red-700');
 
-    return element;
+  return element;
 }
 
 document.body.appendChild(component());
